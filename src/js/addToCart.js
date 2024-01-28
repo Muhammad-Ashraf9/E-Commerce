99f32b83834441724dbd7f1b9322f59cd6f2d4a5
MuhammedAlQamary <muhammedalqamary@gmail.com>
2024-01-28 17:16:32 +0200

    almafroud ghlaaaaast

--- src/js/addToCart.js
+++ src/js/addToCart.js
@@ -11,32 +11,33 @@ let state = JSON.parse(localStorage.getItem("state"));
 export function addtoCart(e) {
   if (e.target.nodeName == "I" || e.target.nodeName == "BUTTON") {
     var prodID = e.target.id;
-    console.log(state);
     let product = state["products"].find((prod) => prod["id"] == prodID);
     let element =
       e.target.parentNode.parentNode.parentNode.parentNode.getElementsByClassName(
         "quantity"
       )[0];
-    console.log(element);
-    console.log(prodID);
     if (state.currentUser) {
       let cart = state.currentUser.cart;
-      console.log(cart);
       let flag = false;
+      let targetI = -1;
       for (let ii = 0; ii < cart.length; ii++) {
         if (cart[ii].id == prodID) {
-          if (cart[ii].quantity > product.stock) {
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "Sorry, there is no more of this item in the seller's stock",
-            });
-            return;
-          }
           cart[ii].quantity++;
           flag = true;
+          targetI = ii;
+          break;
         }
       }
+      if (flag == true && cart[targetI].quantity > product.stock) {
+        cart[targetI].quantity--;
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Sorry, there is no more of this item in the seller's stock",
+        });
+        return;
+      }
+
       if (!flag) {
         cart.push({ id: parseInt(prodID), quantity: 1 });
       }
@@ -52,20 +53,23 @@ export function addtoCart(e) {
       let cartGuest = state.guestCart;
       console.log(cartGuest);
       let flag = false;
+      let targetI = -1;
       for (let ii = 0; ii < cartGuest.length; ii++) {
         if (cartGuest[ii].id == prodID) {
-          if (cart[ii].quantity > product.stock) {
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "Sorry, there is no more of this item in the seller's stock",
-            });
-            return;
-          }
-
           cartGuest[ii].quantity++;
           flag = true;
+          targetI = ii;
+          break;
         }
+      }
+      if (flag == true && cartGuest[targetI].quantity > product.stock) {
+        cartGuest[targetI].quantity--;
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Sorry, there is no more of this item in the seller's stock",
+        });
+        return;
       }
       if (!flag) {
         cartGuest.push({ id: parseInt(prodID), quantity: 1 });
